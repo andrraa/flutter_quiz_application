fbf7b3c626e903cde1ec38a6c29bfb8a2e1f3982
andrraa <andraramadanp@outlook.com>
2023-11-28 09:33:25 +0700

    chore: Get rid of initState

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_application/quiz.dart';
-import 'package:quiz_application/start_screen.dart';
 
 void main(List<String> args) {
   runApp(
